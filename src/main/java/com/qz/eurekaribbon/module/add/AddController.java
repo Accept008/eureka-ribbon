87fe39abddb8530a57e1e5ae5e153f9ddd111c2b
Accept008 <1550581389@qq.com>
2019-11-26 09:16:16 +0800

    改为eureka-feign

--- src/main/java/com/qz/eurekaribbon/module/add/AddController.java
+++ src/main/java/com/qz/eurekaribbon/module/add/AddController.java
@@ -1,5 +1,6 @@
 package com.qz.eurekaribbon.module.add;
 
+import com.qz.eurekaribbon.module.add.service.ComputeClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,12 +9,16 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class AddController {
+//    @Autowired
+//    RestTemplate restTemplate;
+
     @Autowired
-    RestTemplate restTemplate;
+    private ComputeClient computeClient;
 
     @RequestMapping(value = "/add", method = RequestMethod.GET)
-    public String add() {
-        return restTemplate.getForEntity("http://COMPUTE-SERVICE/add?a=10&b=20", String.class).getBody();
+    public Integer add() {
+        //return restTemplate.getForEntity("http://COMPUTE-SERVICE/add?a=10&b=20", String.class).getBody();
+        return computeClient.add(10, 20);
     }
 
 }
